7b427812a01a93aa350ef91a0f723a672d093018
Ulises Cardenas <ulises.card@gmail.com>
2014-11-30 16:16:28 -0600

    init

---
+++ js/walker.js
@@ -0,0 +1,8 @@
+
+
+var Walker = function () {
+  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var cube = new THREE.Mesh( geometry, material );
+
+};
