caecdec745bc10b86fc7155dc23721081ebd4635
Ulises Cardenas <ulises.card@gmail.com>
2014-11-30 16:20:23 -0600

    walker classs

--- js/walker.js
+++ js/walker.js
@@ -1,8 +1,8 @@
 
 
 var Walker = function () {
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
+  this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  this.cube = new THREE.Mesh( geometry, material );
 
 };
